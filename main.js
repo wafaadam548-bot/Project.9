faa63e599e71074b2149f2e93fb95f204ab21d36
Wafa Adam <wafaadam548@gmail.com>
2025-10-05 21:14:59 -0400

    make Dupamicaffeine web

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* this one for the windoes to wait onlode*/
 window.onload =function(){
-    window.alert("hellow  in this web have funn my frendo");
-    document.write("hellow ");
+    //window.alert("hellow  in this web have funn my frendo");
+    //document.write("hellow ");
     //the console is the ptogrammer page
     console.log("%cwafa ya bt","color:red;font-size:40px");
     console.error("don't forget your prayer");
@@ -10,7 +10,12 @@ var myname ="wafa";//var=varyables there is also let and conset are the same as 
 console.log("my name"+ myname);
 //we also have data type intro  
 console.log("wafa");
+let a ="wafaadam";
+let b="noreen";
+//document.write(a+" " +b);
+
 console.log(typeof"wafa");//string
+//we can write the /n to let one line and / for space
 console.log("16");
 console.log(typeof 16);//number
 console.log(typeof ["wafa","adam","noreen"]);//Array
